984dc52ed2c82d7c3db3f04df3b1feeb038b06e1
77Rob <r.cekanauskasnl@gmail.com>
2023-03-20 09:36:11 +0200

    Remove unused code

--- src/firestore/firestore.providers.ts
+++
@@ -1,7 +0,0 @@
-import { UsersDocument } from "src/users/entities/user.document";
-
-export const FirestoreDatabaseProvider = "firestoredb";
-export const FirestoreOptionsProvider = "firestoreOptions";
-export const FirestoreCollectionProviders: string[] = [
-  UsersDocument.collectionName,
-];
